32bb39e1b9ab4153bc4d725c5f0621ad5e2699b2
Corey M. Wood <fourwood@users.noreply.github.com>
2019-03-22 18:06:14 -0500

    [#3]: Removed almost all logic, added first test.

--- src/lib.rs
+++ src/lib.rs
@@ -5,50 +5,6 @@ use std::io::{BufRead, BufReader};
 mod particle;
 use particle::Particle;
 
-fn calculate_pe(particles: &Vec<Particle>) -> f64 {
-    let mut pe = 0.0;
-
-    for i in 0..particles.len() {
-        for j in (i+1)..particles.len() {
-            let dr = particles[j].get_distance(&particles[i]);
-            pe -= particles[i].mass * particles[j].mass / dr;
-        }
-    }
-
-    pe
-}
-
-fn calculate_ke(particles: &Vec<Particle>) -> f64 {
-    let mut ke = 0.0;
-
-    for p in particles {
-        ke += p.calculate_ke();
-    }
-
-    ke
-}
-
-fn update_accelerations(particles: &mut Vec<Particle>) {
-    for i in 0..particles.len() {
-        particles[i].acc0 = particles[i].acc;
-        particles[i].acc = [0f64, 0f64, 0f64];
-
-        for j in 0..particles.len() {
-            if i == j { continue; }
-
-            let dr = [particles[j].pos[0] - particles[i].pos[0],
-                      particles[j].pos[1] - particles[i].pos[1],
-                      particles[j].pos[2] - particles[i].pos[2]];
-
-            let dr_cubed = (dr[0]*dr[0] + dr[1]*dr[1] + dr[2]*dr[2]).powf(1.5);
-
-            particles[i].acc[0] += particles[j].mass * dr[0] / dr_cubed;
-            particles[i].acc[1] += particles[j].mass * dr[1] / dr_cubed;
-            particles[i].acc[2] += particles[j].mass * dr[2] / dr_cubed;
-        }
-    }
-}
-
 pub struct Config {
     pub filename: String,
 }
@@ -65,32 +21,52 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // TODO: Panics if you don't give command line input
-    let input_path = config.filename;
-    println!("Reading n-body input file from {}", input_path);
+fn calculate_pe(particles: &Vec<Particle>) -> f64 {
+    let mut pe = 0.0;
 
-    // Read the input file into a vector of structs
-    let f = fs::File::open(input_path)?;
+    pe
+}
+
+fn calculate_ke(particles: &Vec<Particle>) -> f64 {
+    let mut ke = 0.0;
+
+    ke
+}
+
+fn update_accelerations(particles: &mut Vec<Particle>) -> () {
+}
+
+fn load_state_from_file(filename: &String) -> Vec<Particle> {
+    let f = fs::File::open(filename).expect("Failed to open file");
     let buf = BufReader::new(f);
     let mut particles: Vec<Particle> = Vec::new();
     for line in buf.lines() {
         let line = line.expect("Unable to read line from file");
         let fields: Vec<&str> = line.split_whitespace().collect();
         if fields.len() > 0 {
-            let m: f64 = fields[1].parse().expect("Failed to parse input value");
-            let x: f64 = fields[2].parse().expect("Failed to parse input value");
-            let y: f64 = fields[3].parse().expect("Failed to parse input value");
-            let z: f64 = fields[4].parse().expect("Failed to parse input value");
-            let vx: f64 = fields[5].parse().expect("Failed to parse input value");
-            let vy: f64 = fields[6].parse().expect("Failed to parse input value");
-            let vz: f64 = fields[7].parse().expect("Failed to parse input value");
+            let m: f64 = fields[1].parse().expect("Unable to parse particle mass!");
+            let x: f64 = fields[2].parse().expect("Unable to parse particle x-position!");
+            let y: f64 = fields[3].parse().expect("Unable to parse particle y-position!");
+            let z: f64 = fields[4].parse().expect("Unable to parse particle z-position!");
+            let vx: f64 = fields[5].parse().expect("Unable to parse particle x-velocity!");
+            let vy: f64 = fields[6].parse().expect("Unable to parse particle y-velocity!");
+            let vz: f64 = fields[7].parse().expect("Unable to parse particle z-velocity!");
             let pos = [x, y, z];
             let vel = [vx, vy, vz];
             let p = Particle::new(m, pos, vel);
             particles.push(p);
         }
     }
+    particles
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // TODO: Panics if you don't give command line input
+    let input_path = config.filename;
+    println!("Reading n-body input file from {}", input_path);
+
+    // Read the input file into a vector of structs
+    let mut particles = load_state_from_file(&input_path);
 
     // Setup the time steps
     let t0 = 0.0;
@@ -99,31 +75,29 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let t_max = 1.0;
     let mut steps = 1;
 
-    let ke0 = calculate_ke(&particles);
-    let pe0 = calculate_pe(&particles);
-    let e0 = ke0 + pe0;
+    // TODO: Calculate starting energy
+    let E_0: f64 = 0.0;
+    let PE_0: f64 = 0.0;
+    let KE_0: f64 = 0.0;
     println!("Timestep {}:", steps);
-    println!("E: {:.5}\tKE: {:.5}\tPE: {:.5}", e0, ke0, pe0);
+    println!("E: {:.5}\tKE: {:.5}\tPE: {:.5}", E_0, KE_0, PE_0);
 
-    update_accelerations(&mut particles);
+    // TODO: Set initial accelerations
 
     while t <= t_max {
-        for p in particles.iter_mut() {
-            p.update_position(dt);
-        }
+        // TODO: Update particle positions
 
-        update_accelerations(&mut particles);
+        // TODO: Update particle accelerations (& a->a0)
 
-        for p in particles.iter_mut() {
-            p.update_velocity(dt);
-        }
+        // TODO: Update particle velocities
 
         if steps % 10 == 0 {
-            let ke = calculate_ke(&particles);
-            let pe = calculate_pe(&particles);
-            let e = ke+pe;
+            // TODO: Calculate total system energy
+            let E: f64 = 0.0;
+            let KE: f64 = 0.0;
+            let PE: f64 = 0.0;
             println!("Timestep {}:", steps);
-            println!("E: {:.5}\tKE: {:.5}\tPE: {:.5}\tdE: {:+.5e}", e, ke, pe, (e-e0)/e0);
+            println!("E: {:.5}\tKE: {:.5}\tPE: {:.5}\tdE: {:+.5e}", E, KE, PE, (E-E_0)/E_0);
         }
 
         t += dt;
@@ -131,4 +105,46 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_input() {
+        // The entirety of the input16 file, with unused id set to a float:
+        let input16 = [
+            [-1.0, 0.0625, 0.214835865608654947, -0.120469325327368135, -0.2661811268232539, 0.757849421156586822, 0.157644506165220161, -0.0715602005208729741],
+            [-1.0, 0.0625, -0.129919123158631022, 0.116379094303405983, 0.133080554128754053, 0.486052966602537106, 0.395427627261877745, -0.734692224692122475],
+            [-1.0, 0.0625, -0.410208378045080091, 0.109354870524110206, -0.324396822089431269, -0.383791028776759968, 0.0496602615700619254, 0.378808792416775286],
+            [-1.0, 0.0625, 0.255086977437966045, -0.123206701489244466, -0.370490384977665566, -0.450466427040683792, -0.281691172697528636, -0.185462114144705426],
+            [-1.0, 0.0625, -0.354752159898959474, -0.31060829606615864, 0.314540789088777084, 0.275159898604911246, -0.548791386040452633, -0.209797996849041768],
+            [-1.0, 0.0625, 0.16502984445788807, -0.21452299393747018, 1.32555928694859393, 0.140593096267161405, 0.23623605142486559, 0.273061388949567607],
+            [-1.0, 0.0625, 0.429578710749940917, 0.486818153487116934, 0.181073664985952287, -0.0539885959200906915, -0.176655306840287851, -0.69464977663931049],
+            [-1.0, 0.0625, -0.615104941591427701, 0.297292071802583491, -0.407208633089183814, 0.144247206691717561, 0.880157154616842852, 0.372927641740506965],
+            [-1.0, 0.0625, 0.526844391659375466, -0.225238863848920445, -0.0858175235997876429, -0.170693379896528441, 0.525004419487255625, -0.165895533066861667],
+            [-1.0, 0.0625, -0.784247975459690938, 2.88024285713089023, 0.422769785428553202, -0.397599351887202745, -0.193469722164481828, 0.129716098638342187],
+            [-1.0, 0.0625, 0.557328942603933286, -0.669584458021703521, -1.18669004231490538, -0.193956120455493958, 0.318551106618286173, -0.0385091511689474308],
+            [-1.0, 0.0625, 0.663045405926924847, -1.45465317533174687, 1.08382405457320763, -0.256111120227146827, -0.202605323424895567, 0.288501494317296991],
+            [-1.0, 0.0625, -0.227323512866007577, 0.348104100173003139, -0.147906561326105135, 0.718552359009200226, -0.0282296194471982724, 0.900931748619264838],
+            [-1.0, 0.0625, -0.0756982740351159478, 0.150819419760155976, -0.408069797108747689, -0.0568988462176753251, -0.970986937859766863, -0.0214726046192551326],
+            [-1.0, 0.0625, 0.466554170775371646, -0.914680937590023668, 0.279919799571218153, -0.499730803777436394, -0.201272413171611736, 0.313715970489572504],
+            [-1.0, 0.0625, -0.681049944165142307, -0.356045815568630075, -0.544007043395975831, -0.0592192741330963002, 0.0410207545018133196, -0.535623533470209079],
+        ];
+
+        let filename = String::from("input/input16");
+        let mut particles: Vec<Particle> = load_state_from_file(&filename);
+
+        for (i, p) in particles.iter().enumerate() {
+            let mass = input16[i][1];
+            let pos = [input16[i][2], input16[i][3], input16[i][4]];
+            let vel = [input16[i][5], input16[i][6], input16[i][7]];
+            assert_eq!(p.mass, mass);
+            assert_eq!(p.pos, pos);
+            assert_eq!(p.vel, vel);
+            assert_eq!(p.acc, [0.0, 0.0, 0.0]);
+            assert_eq!(p.acc0, [0.0, 0.0, 0.0]);
+        }
+    }
 }
--- src/particle.rs
+++ src/particle.rs
@@ -20,26 +20,16 @@ impl Particle {
     }
 
     pub fn update_position(&mut self, dt: f64) -> () {
-        self.pos[0] += (self.vel[0] * dt) + (0.5 * self.acc[0] * dt * dt);
-        self.pos[1] += (self.vel[1] * dt) + (0.5 * self.acc[1] * dt * dt);
-        self.pos[2] += (self.vel[2] * dt) + (0.5 * self.acc[2] * dt * dt);
     }
 
     pub fn update_velocity(&mut self, dt: f64) -> () {
-        self.vel[0] += 0.5 * (self.acc[0] + self.acc0[0]) * dt;
-        self.vel[1] += 0.5 * (self.acc[1] + self.acc0[1]) * dt;
-        self.vel[2] += 0.5 * (self.acc[2] + self.acc0[2]) * dt;
     }
 
     pub fn calculate_ke(&self) -> f64 {
-        let v2 = self.vel[0].powi(2) + self.vel[1].powi(2) + self.vel[2].powi(2);
-        0.5 * self.mass * v2
+        0.0
     }
 
     pub fn get_distance(&self, other: &Particle) -> f64 {
-        let dx = other.pos[0] - self.pos[0];
-        let dy = other.pos[1] - self.pos[1];
-        let dz = other.pos[2] - self.pos[2];
-        (dx*dx + dy*dy + dz*dz).sqrt()
+        0.0
     }
 }
